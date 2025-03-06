1cab181d1fadae1c05358b085f0966bfede1c42e
REJvM <everardus.josephus@gmail.com>
2025-03-06 21:28:45 +0200

    Add active color to header buttons

--- components/header.vue
+++ components/header.vue
@@ -1,12 +1,16 @@
+<script setup>
+const route = useRoute()
+</script>
+
 <template>
-    <section class="navigation">
+    <section class="navigation" :class="route.name">
         <div id="container">
             <header>
                 <nav>
-                    <NuxtLink to="/projects" class="menu">Projects</NuxtLink>
-                    <NuxtLink to="/area" class="menu">Area</NuxtLink>
-                    <NuxtLink to="/resources" class="menu">Resources</NuxtLink>
-                    <NuxtLink to="/archive" class="menu">Archive</NuxtLink>
+                    <NuxtLink to="/projects" class="menu" activeClass="active">Projects</NuxtLink>
+                    <NuxtLink to="/area" class="menu" activeClass="active">Area</NuxtLink>
+                    <NuxtLink to="/resources" class="menu" activeClass="active">Resources</NuxtLink>
+                    <NuxtLink to="/archive" class="menu" activeClass="active">Archive</NuxtLink>
                 </nav>
                 <NuxtLink to="/login" class="menu secondary">Login</NuxtLink>
             </header>

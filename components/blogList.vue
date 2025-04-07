3367edead2a9b45993a4b3379eebd9e7863c566b
REJvM <everardus.josephus@gmail.com>
2025-04-07 22:00:13 +0300

    Disable problematic formatDate function

--- components/blogList.vue
+++ components/blogList.vue
@@ -1,37 +1,33 @@
 <script setup>
 const props = defineProps({
-    items: Array,
-    apiStatus: String
-})
+  items: Array,
+  apiStatus: String,
+});
 </script>
 
 <template>
-    <div v-if="items.length > 0">
-        <ul>
-            <li 
-                v-for="item in items" 
-                :key="item.id"
-                :class="item.category"
-            >        
-                <NuxtLink 
-                    :to="{ 
-                        path: '/blog-posts/' + item.id, 
-                        query: { category: item.category } 
-                    }"
-                >
-                    {{ item.title }}
-                    <span class="modifiedDate">{{ formatDate(item.last_modified_on) }}</span>
-                </NuxtLink>
-            </li>
-        </ul>
-    </div>
-    <div v-else-if="apiStatus === 'pending'">
-        <p>Loading...</p>
-    </div>
-    <div v-else-if="apiStatus !== 'success'">
-        <p>There has been an error loading the blog posts. Try again later.</p>
-    </div>
-    <div v-else-if="items.length === 0">
-        <p>No blog posts have been found.</p>
-    </div>
+  <div v-if="items.length > 0">
+    <ul>
+      <li v-for="item in items" :key="item.id" :class="item.category">
+        <NuxtLink
+          :to="{
+            path: '/blog-posts/' + item.id,
+            query: { category: item.category },
+          }"
+        >
+          {{ item.title }}
+          <!-- <span class="modifiedDate">{{ formatDate(item.last_modified_on) }}</span> -->
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+  <div v-else-if="apiStatus === 'pending'">
+    <p>Loading...</p>
+  </div>
+  <div v-else-if="apiStatus !== 'success'">
+    <p>There has been an error loading the blog posts. Try again later.</p>
+  </div>
+  <div v-else-if="items.length === 0">
+    <p>No blog posts have been found.</p>
+  </div>
 </template>
--- pages/blog-posts/[id].vue
+++ pages/blog-posts/[id].vue
@@ -1,59 +1,60 @@
 <script setup>
-const { $api } = useNuxtApp()
-const blogPostRepository = repository($api)
-const route = useRoute()
-const config = useRuntimeConfig()
-const picturePath = config.public.picturePath
+const { $api } = useNuxtApp();
+const blogPostRepository = repository($api);
+const route = useRoute();
+const config = useRuntimeConfig();
+const picturePath = config.public.picturePath;
 
-const { data: blogPost, status } = await useAsyncData(
-    () => blogPostRepository.getSingleBlogpost(route.params.id)
-)
+const { data: blogPost, status } = await useAsyncData(() =>
+  blogPostRepository.getSingleBlogpost(route.params.id)
+);
 </script>
 
 <template>
-    <template v-if="blogPost !== null">
-        <section class="blog blog-post" :class="blogPost.category ?? ''">
-            <div v-if="status === 'pending'">
-                <p>Loading...</p>
-            </div>
-            <div v-else-if="status !== 'success'">
-                <p>There has been an error loading the blog posts. Try again later.</p>
-            </div>
-            <div v-else-if="blogPost === null">
-                <p>No blog posts have been found.</p>
-            </div>
-            <div v-else>
-                <Breadcrumbs
-                    :category="blogPost.category ?? ''"
-                    :pageTitle="blogPost.title"
-                />
-                <header>
-                    <h1>
-                        {{ blogPost.title }}
-                    </h1>
-                    
-                    <div v-if="blogPost.user" class="blogpost-user">
-                        <img v-if="blogPost.user.picture" :src="picturePath + blogPost.user.picture" />
-                        <div>
-                            <p>{{ blogPost.user.name }}</p>
-                            <p>Last modified: {{ formatDate(blogPost.lastModifiedOn) }}</p>
-                        </div>
-                    </div>
-                    <div v-else>
-                        <p>Last modified: {{ formatDate(blogPost.lastModifiedOn) }}</p>
-                    </div>
-                </header>
-                <div class="content">
-                    <article v-html="blogPost.content" ></article>
-                    <TableOfContent
-                        :content="blogPost.content ?? ''"
-                        :links="blogPost.links ?? ''"
-                    />
-                </div>
-            </div>
-        </section>
-        <RouteTransition
-            :blogPostCategory="blogPost.category"
+  <template v-if="blogPost !== null">
+    <section class="blog blog-post" :class="blogPost.category ?? ''">
+      <div v-if="status === 'pending'">
+        <p>Loading...</p>
+      </div>
+      <div v-else-if="status !== 'success'">
+        <p>There has been an error loading the blog posts. Try again later.</p>
+      </div>
+      <div v-else-if="blogPost === null">
+        <p>No blog posts have been found.</p>
+      </div>
+      <div v-else>
+        <Breadcrumbs
+          :category="blogPost.category ?? ''"
+          :pageTitle="blogPost.title"
         />
-    </template>
+        <header>
+          <h1>
+            {{ blogPost.title }}
+          </h1>
+
+          <div v-if="blogPost.user" class="blogpost-user">
+            <img
+              v-if="blogPost.user.picture"
+              :src="picturePath + blogPost.user.picture"
+            />
+            <div>
+              <p>{{ blogPost.user.name }}</p>
+              <!-- <p>Last modified: {{ formatDate(blogPost.lastModifiedOn) }}</p> -->
+            </div>
+          </div>
+          <div v-else>
+            <!-- <p>Last modified: {{ formatDate(blogPost.lastModifiedOn) }}</p> -->
+          </div>
+        </header>
+        <div class="content">
+          <article v-html="blogPost.content"></article>
+          <TableOfContent
+            :content="blogPost.content ?? ''"
+            :links="blogPost.links ?? ''"
+          />
+        </div>
+      </div>
+    </section>
+    <RouteTransition :blogPostCategory="blogPost.category" />
+  </template>
 </template>
